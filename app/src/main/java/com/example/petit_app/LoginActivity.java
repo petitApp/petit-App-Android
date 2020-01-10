910c0f717c4d8397e3958b04373f3fbffeb45a42
Aitor <aitor.ballesteros.pavon@gmail.com>
2020-01-10 19:12:13 +0100

    Validation Register and Login

--- app/src/main/java/com/example/petit_app/LoginActivity.java
+++ app/src/main/java/com/example/petit_app/LoginActivity.java
@@ -3,11 +3,14 @@ package com.example.petit_app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -33,51 +36,57 @@ public class LoginActivity extends AppCompatActivity {
         super.onResume();
 
 
-
         //Click Listener del botón del login
-        loginBtn.setOnClickListener(new View.OnClickListener(){
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = input_email.getText().toString();
                 String pass = input_password.getText().toString();
-
-                checkInputs(email , pass);
+                checkInputs(email, pass);
             }
         });
 
     }
 
     //Method to check inputs of the login view
-    private void checkInputs(String email ,String pass){
+    private void checkInputs(String email, String pass) {
 
         //Check if inputs are empty
         if (email.isEmpty() || pass.isEmpty()) {
-            (Toast.makeText(getApplicationContext() , "Empty inputs are not allowed", Toast.LENGTH_LONG)).show();
+            (Toast.makeText(getApplicationContext(), "Empty inputs are not allowed", Toast.LENGTH_LONG)).show();
         } else {
-            checkEmail(email);
-            checkPass(pass);
+            if (checkEmail(email) == true && checkPass(pass) == true) {
+                (Toast.makeText(getApplicationContext(), "Welcome to Pet it", Toast.LENGTH_LONG)).show();
+            }
         }
-
     }
 
-    private void checkEmail(String email){
+    private boolean checkEmail(String email) {
         //Check email comparing to the email that arrived through parameters
-        email.matches("\"^(.+)@(.+)$\"");
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            (Toast.makeText(getApplicationContext(), "Introduce a correct email", Toast.LENGTH_LONG)).show();
+            return false;
+
+        }
+        return true;
     }
 
-    private void checkPass(String pass){
+    private boolean checkPass(String pass) {
 
-        if(pass.length() <= 8){
-            (Toast.makeText(getApplicationContext() , "The password must be greater than 8 digits", Toast.LENGTH_LONG)).show();
+        if (pass.length() <= 8) {
+            (Toast.makeText(getApplicationContext(), "The password must be greater than 8 digits", Toast.LENGTH_LONG)).show();
+            return false;
         }
-        if(!pass.matches("(?=.*[0-9]).*")){
-            (Toast.makeText(getApplicationContext() , "The password must contains at least one digit", Toast.LENGTH_LONG)).show();
+        if (!pass.matches("(?=.*[0-9]).*")) {
+            (Toast.makeText(getApplicationContext(), "The password must contains at least one number", Toast.LENGTH_LONG)).show();
+            return false;
         }
-        if(!pass.matches("(?=.*[A-Z]).*")){
-            (Toast.makeText(getApplicationContext() , "The password must contains one upper case letter", Toast.LENGTH_LONG)).show();
+        if (!pass.matches("(?=.*[A-Z]).*")) {
+            (Toast.makeText(getApplicationContext(), "The password must contains one upper case letter", Toast.LENGTH_LONG)).show();
+            return false;
         }
 
-
+        return true;
     }
 
     //Click from login to register view
--- app/src/main/java/com/example/petit_app/RegisterActivity.java
+++ app/src/main/java/com/example/petit_app/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.petit_app;
 import android.app.AppComponentFactory;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,7 +18,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class RegisterActivity extends AppCompatActivity {
 
     EditText input_email, input_password, input_username, input_name, input_address, input_phone_number;
-    Button loginBtn;
+    Button btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,17 +31,15 @@ public class RegisterActivity extends AppCompatActivity {
         input_name = findViewById(R.id.input_name);
         input_address = findViewById(R.id.input_address);
         input_phone_number = findViewById(R.id.input_phone_number);
-        loginBtn = findViewById(R.id.btn_login);
+        btn_register = findViewById(R.id.btn_register);
     }
 
 
     protected void onResume() {
         super.onResume();
 
-
-
         //Click Listener del botón del login
-        loginBtn.setOnClickListener(new View.OnClickListener(){
+        btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = input_email.getText().toString();
@@ -48,52 +47,62 @@ public class RegisterActivity extends AppCompatActivity {
                 String username = input_username.getText().toString();
                 String name = input_name.getText().toString();
                 String address = input_address.getText().toString();
-                int phone_number = Integer.parseInt(input_phone_number.getText().toString());
+                String phone_number = input_phone_number.getText().toString();
 
-                checkInputs(email , pass , username, name, address, phone_number);
+                checkInputs(email, pass, username, name, address, phone_number);
             }
         });
     }
 
     //Method to check inputs of the login view
-    private void checkInputs(String email ,String pass, String username, String name, String address, int phone_number ){
+    private void checkInputs(String email, String pass, String username, String name, String address, String phone_number) {
 
         //Check if inputs are empty
-        if (email.isEmpty() || pass.isEmpty() || username.isEmpty() || name.isEmpty()|| address.isEmpty()) {
-            (Toast.makeText(getApplicationContext() , "Empty inputs are not allowed", Toast.LENGTH_LONG)).show();
+        if (email.isEmpty() || pass.isEmpty() || username.isEmpty() || name.isEmpty() || address.isEmpty()) {
+            (Toast.makeText(getApplicationContext(), "Empty inputs are not allowed", Toast.LENGTH_LONG)).show();
         } else {
-            checkEmail(email);
-            checkPass(pass);
-            checkPhoneNumber(phone_number);
+
+            if (checkEmail(email) == true && checkPass(pass) == true && checkPhoneNumber(phone_number)) {
+                (Toast.makeText(getApplicationContext(), "Welcome to Pet it", Toast.LENGTH_LONG)).show();
+            }
         }
 
     }
 
-    private void checkEmail(String email){
+    private boolean checkEmail(String email) {
         //Check email comparing to the email that arrived through parameters
-        email.matches("\"^(.+)@(.+)$\"");
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            (Toast.makeText(getApplicationContext(), "Introduce a correct email", Toast.LENGTH_LONG)).show();
+            return false;
+        }
+        return true;
     }
 
-    private void checkPass(String pass){
+    private boolean checkPass(String pass) {
+        if (pass.length() <= 8) {
+            (Toast.makeText(getApplicationContext(), "The password must be greater than 8 digits", Toast.LENGTH_LONG)).show();
+            return false;
+        }
+        if (!pass.matches("(?=.*[0-9]).*")) {
+            (Toast.makeText(getApplicationContext(), "The password must contains at least one number", Toast.LENGTH_LONG)).show();
+            return false;
+        }
+        if (!pass.matches("(?=.*[A-Z]).*")) {
+            (Toast.makeText(getApplicationContext(), "The password must contains one upper case letter", Toast.LENGTH_LONG)).show();
+            return false;
+        }
 
-        if(pass.length() <= 8){
-            (Toast.makeText(getApplicationContext() , "The password must be greater than 8 digits", Toast.LENGTH_LONG)).show();
-        }
-        if(!pass.matches("(?=.*[0-9]).*")){
-            (Toast.makeText(getApplicationContext() , "The password must contains at least one digit", Toast.LENGTH_LONG)).show();
-        }
-        if(!pass.matches("(?=.*[A-Z]).*")){
-            (Toast.makeText(getApplicationContext() , "The password must contains one upper case letter", Toast.LENGTH_LONG)).show();
-        }
+        return true;
     }
 
-    private void checkPhoneNumber(int phoneNumber){
-        if(phoneNumber >= 9){
-            (Toast.makeText(getApplicationContext() , "The phone number must be greater than 9 digits", Toast.LENGTH_LONG)).show();
+    private boolean checkPhoneNumber(String phoneNumber) {
+        int number = Integer.parseInt(phoneNumber);
 
+        if (number >= 9) {
+            (Toast.makeText(getApplicationContext(), "The Phone Number must be greater than 9 digits", Toast.LENGTH_LONG)).show();
+            return false;
         }
+        return true;
     }
 
 }
-
-
