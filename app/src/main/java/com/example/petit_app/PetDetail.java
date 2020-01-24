7460b2d86496d02dee20422e4aa2abc4256dbc78
Diana Onciu <dianaonciu@gmail.com>
2020-01-24 18:03:32 +0100

    Pet details info request

--- app/src/main/java/com/example/petit_app/PetDetail.java
+++ app/src/main/java/com/example/petit_app/PetDetail.java
@@ -1,17 +1,62 @@
 package com.example.petit_app;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class PetDetail extends AppCompatActivity {
 
+
+    TextView namePet, location, distance, detailAge, detailBreed;
+
+    private APIService APIService;
+    Context context;
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pet_detail);
 
+        namePet = findViewById(R.id.namePet);
+        location = findViewById(R.id.location);
+        distance = findViewById(R.id.distance);
+        detailAge = findViewById(R.id.detailAge);
+        detailBreed = findViewById(R.id.detailBreed);
+        APIService = ApiUtils.getAPIService();
 
+        getAnimalInfo();
     }
+
+
+    private void getAnimalInfo()
+    {
+        Call<String> call = APIService.getInfo();
+        call.enqueue(new Callback<String>(){
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                    String petName = response.body();
+                    namePet.setText(petName);
+
+                    Log.d("RESPONSE_OK", "posts loaded from API");
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                Log.d("RESPONSE_FAILURE", "error loading from API");
+
+            }
+        });
+    }
+
 }
