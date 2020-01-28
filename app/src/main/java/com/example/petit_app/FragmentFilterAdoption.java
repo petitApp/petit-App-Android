04bfeb9287d5cd6d3911f9f868a56e560d83a20d
Diana Onciu <dianaonciu@gmail.com>
2020-01-28 20:10:16 +0100

    Hidden seekbar

--- app/src/main/java/com/example/petit_app/FragmentFilterAdoption.java
+++ app/src/main/java/com/example/petit_app/FragmentFilterAdoption.java
@@ -15,6 +15,7 @@ import android.widget.GridView;
 import android.widget.LinearLayout;
 
 import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 
@@ -26,8 +27,9 @@ public class FragmentFilterAdoption extends Fragment {
 
     AdapterImagesPets adapterImagesPets;
     GridView gridView;
+    TabLayout tabs;
     TabItem ageFilter,distanceFilter, raceFilter;
-    LinearLayout dogFilterButton, catFilterButton, otherPetFilterButton;
+    LinearLayout dogFilterButton, catFilterButton, otherPetFilterButton, seekBarFilter;
     ArrayList<Animal> elementItemArray = new ArrayList<>();
     MainActivity mainActivity  = (MainActivity) getActivity();
 
@@ -37,10 +39,12 @@ public class FragmentFilterAdoption extends Fragment {
         View RootView = inflater.inflate(R.layout.fragment_filter_adoption, container, false);
 
         gridView = (GridView) RootView.findViewById((R.id.gridView));
+        tabs = (TabLayout) RootView.findViewById((R.id.tabs));
         ageFilter = (TabItem) RootView.findViewById((R.id.ageFilter));
         distanceFilter = (TabItem) RootView.findViewById((R.id.distanceFilter));
         raceFilter = (TabItem) RootView.findViewById((R.id.raceFilter));
 
+        seekBarFilter = (LinearLayout) RootView.findViewById((R.id.seekBarFilter));
         otherPetFilterButton = (LinearLayout) RootView.findViewById((R.id.otherPetFilterButton));
         catFilterButton = (LinearLayout) RootView.findViewById((R.id.catFilterButton));
         dogFilterButton = (LinearLayout) RootView.findViewById((R.id.dogFilterButton));
@@ -52,11 +56,43 @@ public class FragmentFilterAdoption extends Fragment {
         adapterImagesPets = new AdapterImagesPets(getActivity().getApplicationContext(), R.layout.item_card_pet, elementItemArray);
         gridView.setAdapter(adapterImagesPets);
 
+        seekBarFilter.setVisibility(View.GONE);
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 adapterImagesPets.notifyDataSetChanged();
             }
+        });
+
+
+        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab raceFilter) {
+                switch (tabs.getSelectedTabPosition()) {
+                    case 0:
+                        seekBarFilter.setVisibility(View.GONE);
+                        break;
+
+                    case 1:
+                        seekBarFilter.setVisibility(View.VISIBLE);
+                        break;
+                    case 2:
+                        seekBarFilter.setVisibility(View.GONE);
+                        break;
+
+                }
+
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+               
+            }
+
         });
 
 
