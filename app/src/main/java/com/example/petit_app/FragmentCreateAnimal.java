cb18c9199d32a370cd35f1ba05e7cbfe25dcd216
Aitor <aitor.ballesteros.pavon@gmail.com>
2020-02-09 12:38:05 +0100

    Design Create Animal

--- app/src/main/java/com/example/petit_app/FragmentCreateAnimal.java
+++ app/src/main/java/com/example/petit_app/FragmentCreateAnimal.java
@@ -17,6 +17,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import retrofit2.Call;
@@ -33,8 +34,8 @@ public class FragmentCreateAnimal extends Fragment {
     private APIService APIService;
     Dialog dialogLoading;
     Button btn_add_animal;
-    EditText namePet, detailAge, petDescription, detailBreed;
-
+    EditText namePet, petDescription, detailBreed;
+    SeekBar detailAge;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -45,7 +46,7 @@ public class FragmentCreateAnimal extends Fragment {
         btn_add_animal = (Button) RootView.findViewById(R.id.btn_add_animal);
         namePet = (EditText) RootView.findViewById(R.id.namePet);
         petDescription = (EditText) RootView.findViewById(R.id.petDescription);
-        detailAge = (EditText) RootView.findViewById(R.id.detailAge);
+        detailAge = (SeekBar) RootView.findViewById(R.id.detailAge);
         detailBreed = (EditText) RootView.findViewById(R.id.detailBreed);
 
         btn_add_animal.setOnClickListener(new View.OnClickListener() {
@@ -53,19 +54,19 @@ public class FragmentCreateAnimal extends Fragment {
             public void onClick(View v) {
 
                 String name = namePet.getText().toString();
-                String age = detailAge.getText().toString();
+                String age = String.valueOf(detailAge.getProgress());
                 String type = "Perro";
                 String sex = "Macho";
                 String latitude = "333.333";
                 String longitude = "333.333";
-                String description = petDescription.getText().toString();
+           //     String description = petDescription.getText().toString();
                 String available = "Yes";
                 String id_owner = "1";
-                String picture = "drawable://" + R.drawable.perrito;
+                String picture = "drawable://" + R.drawable.icon_location;
                 String breed = detailBreed.getText().toString();
 
 
-                createAnimal(name, age, type, sex, latitude, longitude, description, available, id_owner, picture, breed);
+           //     createAnimal(name, age, type, sex, latitude, longitude, description, available, id_owner, picture, breed);
 
             }
         });
